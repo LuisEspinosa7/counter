4b27b3a9b8b18c71f5387232d7c930c93e8d5e73
Luis Espinosa 7 <lluisllanoss@gmail.com>
2022-04-01 21:36:05 -0500

    scale effect on counter container added

--- src/components/styles/containers/CounterContainer.styled.js
+++ src/components/styles/containers/CounterContainer.styled.js
@@ -4,6 +4,14 @@ import { StyledGridContainer } from './GridContainer.styled';
 /* This is required when you need to apply media queries to some 
 containers and components */
 export const StyledCounterContainer = styled(StyledGridContainer)`
+
+    -webkit-transition: all ${props => props.transitionSeconds ? props.transitionSeconds : '0.5s'} ease;
+    -moz-transition: all ${props => props.transitionSeconds ? props.transitionSeconds : '0.5s'} ease;
+    transition: all ${props => props.transitionSeconds ? props.transitionSeconds : '0.5s'} ease;
+
+    &:hover {
+        transform: scale(${props => props.scaleNumber ? props.scaleNumber : 1.04});
+    }
     
     /* Media Queries */
     @media all and (max-width: 767px){
