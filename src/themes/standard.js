9e74aea1b886c9327692ae85ccb906cd614449e7
Luis Espinosa 7 <lluisllanoss@gmail.com>
2022-04-01 21:06:47 -0500

    Colors selected based on some UX rules

--- src/themes/standard.js
+++ src/themes/standard.js
@@ -1,13 +1,23 @@
 export const theme = {
     colors: {
         primary: {
-            normal: '#533535',
+            /* HSB: 352.5, 70.97, 97.25  #f8485e */
+            /* HSB: 168, 56.34, 55.69  #3E8E7E */
+            normal: '#3E8E7E',
         },
         secondary: {
-            normal: '#E6CCA9'
+            /* Select a value between 5  and 10 for the S,
+               Add 5 or 10 points to the B */
+            /* HSB: 352.5, 5, 100  #fff2f4 */
+            /* HSB: 168, 8, 100  #ebfffb */
+            normal: '#ebfffb'
         },
         accent: {
-            normal: '#FF6D6D'
+            /* Decrease 30 or 40 points to the H,
+               Add 5 or 10 points to the B */
+            /* HSB: 312.5, 70.97, 100  #ff4ad9 */
+            /* HSB: 128, 56.34, 98  #6dfa80 */
+            normal: '#6dfa80'
         },
         gray: {
             normal: '#EEEEEE',
